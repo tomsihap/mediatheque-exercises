f50ff94c7819cb992561f3fb9101ad49b9eb7353
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2019-06-18 10:44:02 +0200

    :rocket: Ajout d'un média

--- ajout-media.php
+++ ajout-media.php
@@ -5,6 +5,16 @@
 if (!empty($_POST)) {
 
     // Gérer les données du formulaire
+    $query = "  INSERT INTO media (creator, title, type_id)
+                VALUES (:creator, :title, :type_id)";
+
+    $request = $bdd->prepare($query);
+
+    $request->execute([
+        'creator'   => $_POST['creator'],
+        'title'     => $_POST['title'],
+        'type_id'   => $_POST['type_id'],
+    ]);
 
 }
 ?>
@@ -16,7 +26,26 @@ if (!empty($_POST)) {
 <h1>Ajout d'un média</h1>
 
 <!-- FORMULAIRE D'AJOUT -->
+<form action="ajout-media.php" method="post">
 
+    <div class="form-group">
+        <label for="formCreator">Créateur</label>
+        <input name="creator" class="form-control" type="text">
+    </div>
+
+    <div class="form-group">
+        <label for="formTitle">Titre</label>
+        <input name="title" class="form-control" type="text">
+    </div>
+
+    <div class="form-group">
+        <label for="formTypeId"># du type de média</label>
+        <input name="type_id" class="form-control" type="number">
+    </div>
+
+    <button class="btn btn-primary float-right" type="submit">Créer</button>
+
+</form>
 <!-- / FORMULAIRE D'AJOUT -->
 
 <!-- // Inclusion du footer -->
