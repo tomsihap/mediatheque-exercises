7bbd1d2d1017ad5b2db94f6471d0ba9745f9a994
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2019-06-18 10:49:06 +0200

    :rocket: Ajout d'un user

--- ajout-user.php
+++ ajout-user.php
@@ -2,23 +2,44 @@
 
 
 <?php
-
 if (!empty($_POST)) {
 
     // Gérer les données du formulaire
+    $query = "  INSERT INTO user (name, media_id)
+                VALUES (:name, :media_id)";
 
+    $request = $bdd->prepare($query);
+
+    $request->execute([
+        'name'      => $_POST['name'],
+        'media_id'  => $_POST['media_id']
+    ]);
 }
 ?>
 
 <!-- // Inclusion du header -->
 <?php include('partials/_header.php'); ?>
 
-<a href="liste-users.php" class="btn btn-sm btn-secondary">< retour à la liste</a>
-<h1>Ajout d'un user</h1>
+<a href="liste-users.php" class="btn btn-sm btn-secondary">
+    < retour à la liste</a> <h1>Ajout d'un utilisateur</h1>
 
-<!-- FORMULAIRE D'AJOUT -->
+        <!-- FORMULAIRE D'AJOUT -->
+        <form action="ajout-user.php" method="post">
 
-<!-- / FORMULAIRE D'AJOUT -->
+            <div class="form-group">
+                <label for="formName">Nom de l'emprunteur</label>
+                <input name="name" class="form-control" type="text">
+            </div>
 
-<!-- // Inclusion du footer -->
-<?php include('partials/_footer.php'); ?>
+            <div class="form-group">
+                <label for="formMediaId"># du média emprunté</label>
+                <input name="media_id" class="form-control" type="number">
+            </div>
+
+            <button class="btn btn-primary float-right" type="submit">Créer</button>
+
+        </form>
+        <!-- / FORMULAIRE D'AJOUT -->
+
+        <!-- // Inclusion du footer -->
+        <?php include('partials/_footer.php'); ?>
