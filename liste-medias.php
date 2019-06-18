710e72de8462645d45b2a389ad5bbda2776c985f
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2019-06-18 10:39:04 +0200

    :rocket: Affichage de la liste des médias

--- liste-medias.php
+++ liste-medias.php
@@ -4,6 +4,9 @@
 
 <?php
 // Récupérer les données avec PDO
+$request = 'SELECT * FROM media';
+$response = $bdd->query($request);
+$medias = $response->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!-- // Inclusion du header -->
@@ -14,9 +17,24 @@
 <h1>Liste des médias</h1>
 
 <!-- // Foreach du tableau -->
-<?php foreach($a as $b) : ?>
-
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Créateur</th>
+            <th>Titre</th>
+            <th># du type de média</th>
+        </tr>
+    </thead>
+<?php foreach($medias as $media) : ?>
+    <tr>
+        <td><?= $media['id'] ?></td>
+        <td><?= $media['creator'] ?></td>
+        <td><?= $media['title'] ?></td>
+        <td><?= $media['type_id'] ?></td>
+    </tr>
 <?php endforeach; ?>
+</table>
 
 <!-- // Inclusion du footer -->
 <?php include('partials/_footer.php'); ?>
